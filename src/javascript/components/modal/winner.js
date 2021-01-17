b57e623836f8681a97d9304a157823fe8b71d2dd
AndreyUA <infernalsteel666@gmail.com>
2021-01-17 19:29:16 +0200

    Arena fight done

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,17 @@
+import { showModal } from './modal';
+import { createElement } from '../../helpers/domHelper';
+
 export function showWinnerModal(fighter) {
-  // call showModal function 
+  const bodyElement = createElement({ tagName: 'span', className: 'modal-text' });
+  bodyElement.innerText = 'Close this modal window to start a new game!';
+
+  const arg = {
+    title: `The winner is ${fighter.name}`,
+    bodyElement,
+    onClose: () => {
+      location.reload();
+    },
+  };
+
+  showModal(arg);
 }
